0e45e86773a2839ae7c7ab8cd15f8c23ba84f9a7
Miguel <mabreuortega@gmail.com>
2014-03-16 18:04:37 -0400

    Initial commit

---
+++ Source/MyVanity/MyVanity.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyVanity.Web.Startup))]
+namespace MyVanity.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
